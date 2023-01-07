8520e7063c2d0072146aed4c13282b5515d1d3e7
pixelkingliam <79420307+pixelkingliam@users.noreply.github.com>
2023-01-07 10:50:06 -0500

    //SRCS/RESP/accounts.css// Fixed a bug where the server would not output an error to the console or the client when a login attempt used a incorrect username>. Refactored a foreach statement into a more optimized for statement (line 169)

--- SRCS/RESP/accounts.cs
+++ SRCS/RESP/accounts.cs
@@ -60,11 +60,11 @@ namespace Accounts
                     !username.ToLower().Contains("\\") &
                     !username.ToLower().Contains("|") &
                     !username.ToLower().Contains("?") &
-                    !username.ToLower().Contains("*") 
-                    
+                    !username.ToLower().Contains("*")
+
                 )
                 {
-                    string[] forbiddenwords = {"CON","PRN","AUX","NUL","COM1","COM2","COM3","COM4","COM5","COM6","COM7","COM8","COM9","LPT1","LPT2","LPT3","LPT4","LPT5","LPT6","LPT7","LPT8","LPT9"};
+                    string[] forbiddenwords = { "CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9" };
                     if ( // check if the username contains forbidden file/folder name
                         !forbiddenwords.Contains(username)
                     )
@@ -72,31 +72,36 @@ namespace Accounts
                         if (!username.EndsWith(" ") & !username.EndsWith(".")) // Windows doesn't like it when filename ends with these
                         {
                             issafe = true;
-                        }else
+                        }
+                        else
                         {
                             issafe = false;
                         }
-                    }else
+                    }
+                    else
                     {
                         issafe = false;
                     }
-                }else
+                }
+                else
                 {
                     issafe = false;
                 }
-            }else
+            }
+            else
             {
                 if (!username.ToLower().Contains("/"))
                 {
-                issafe = true;
-                }else
+                    issafe = true;
+                }
+                else
                 {
                     issafe = false;
                 }
             }
             if (!AccountExist & issafe)
             {
-                
+
                 Account NewAccount = new Account();
                 NewAccount.username = username;
                 NewAccount.password = Hash.HashString(password);
@@ -121,8 +126,9 @@ namespace Accounts
             else if (!issafe)
             {
                 jsonresults["result"] = 2;
-                Log.Warning(username + "Tried to make an account with illegal characters!");
-            }else if (issafe)
+                Log.Warning(username + " Tried to make an account with illegal characters!");
+            }
+            else if (issafe)
             {
                 jsonresults["result"] = 1;
                 Log.Warning(username + " Tried to make an existing account!");
@@ -157,35 +163,30 @@ namespace Accounts
             //Log.Info
             foreach (var item in jsonaccounts)
             {
-                LoadedAccs.Add(new Account{ username = item["username"].ToString(), password = item["password"].ToString() });
+                LoadedAccs.Add(new Account { username = item["username"].ToString(), password = item["password"].ToString() });
             }
-            int index = 0;
-            foreach (var item in LoadedAccs)
+            jsonresults["key"] = "false";
+            for (int i = 0; i < LoadedAccs.Count;i++)
             {
-                if (item.username == username)
+                
+                if ((Hash.HashString(password) == LoadedAccs[i].password) && (username == LoadedAccs[i].username))
                 {
+                    string pkey = Hash.HashString(Convert.ToString(rnd.Next()));
+                    // private key is used for the game client so users dont have to give password/username all the time
+                    pkeys.Add(pkey, username);
+                    jsonresults["key"] = pkey;
                     break;
-                }else
-                {
-                    index++;
                 }
-            }
-        if ((Hash.HashString(password) == LoadedAccs[index].password))
-            {
-                string pkey = Hash.HashString(Convert.ToString(rnd.Next()));
-                // private key is used for the game client so users dont have to give password/username all the time
-                pkeys.Add(pkey, username);
-                jsonresults["key"] = pkey;
-                //    Log.Success("Test Passed for existing");
-            }
-            else
-            {
-                jsonresults["key"] = "false";
-                Log.Warning("Client failed to login");
+
             }
             jsonresponse["result"] = jsonresults;
             Send(jsonresponse.ToString());
+            if ((string)jsonresults["key"] == "false")
+            {
+                Log.Warning("Client failed to login");
+            }
         }
+        
         protected override void OnError(WebSocketSharp.ErrorEventArgs e)
         {
             Log.Network(e.Message + " This is most likely a client-related issue, the server will keep running.");
@@ -194,12 +195,6 @@ namespace Accounts
     }
     public class VerifyAccount : WebSocketBehavior
     {
-        /*private readonly Login login;
-
-        public VerifyAccount(Login login)
-        {
-        this.login = login;
-        }*/
         protected override void OnMessage(MessageEventArgs e)
         {
             var msg = e.Data;
@@ -212,10 +207,12 @@ namespace Accounts
             string key = (string)jsonpacket.SelectToken("connection.key");
             var jsonresponse = new JObject();
             var jsonresults = new JObject();
-            
-            if (Login.pkeys.ContainsKey(key)){
+
+            if (Login.pkeys.ContainsKey(key))
+            {
                 jsonresults["result"] = 0;
-            }else
+            }
+            else
             {
                 jsonresults["result"] = 1;
             }
